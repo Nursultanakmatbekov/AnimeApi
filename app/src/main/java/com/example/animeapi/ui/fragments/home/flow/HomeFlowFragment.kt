a7da23db8d93f6f54c5572075b426ebbe0b77eea
Nursultanakmatbekov <nursultanakmatbekov@gmail.com>
2023-02-25 00:36:55 +0600

    еще раз запушил 7дз

---
+++ app/src/main/java/com/example/animeapi/ui/fragments/home/flow/HomeFlowFragment.kt
@@ -0,0 +1,10 @@
+package com.example.animeapi.ui.fragments.home.flow
+
+import com.example.animeapi.base.BaseFlowFragment
+import com.excample.animeapp.R
+
+
+class HomeFlowFragment : BaseFlowFragment(R.layout.fragment_home_flow) {
+    override val navigationId: Int
+        get() = R.id.home_host_fragment
+}
---
+++ app/src/main/java/com/example/animeapi/ui/fragments/singup/flow/SingUpFlowFragment.kt
@@ -0,0 +1,9 @@
+package com.example.animeapi.ui.fragments.singup.flow
+
+import com.example.animeapi.base.BaseFlowFragment
+import com.excample.animeapp.R
+
+class SingUpFlowFragment : BaseFlowFragment(R.layout.fragment_sing_up_flow) {
+    override val navigationId: Int
+        get() = R.id.sign_up_host_fragment
+}
